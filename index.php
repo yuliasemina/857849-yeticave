fb96797ffacfef03477198e13ff2d3e8a11e720f
Yulia <semina.yulia@bk.ru>
2019-02-02 22:54:31 +0300

    правки по заданию 4 к лекции 1

--- index.php
+++ index.php
@@ -27,14 +27,14 @@ $user_name = 'Юлия'; // укажите здесь ваше имя
                 <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
                 <nav class="user-menu">
-                    <?php if ($is_auth == 1): ?>
+                    <?php if ($is_auth === 1): ?>
                      <div class="user-menu__logged">
                         <p>
                             <?php print($user_name); ?>
                         </p>
                     </div>
                     
-                    <?php elseif  ($is_auth == 0): ?>
+                    <?php else: ?>
                         <ul class="user-menu__list">
                             <li class="user-menu__item">
                               <a href="#">Регистрация</a>
