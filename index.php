eb0a6487eed28deb0e21a3b77e8eb1d227e070a9
Yulia <semina.yulia@bk.ru>
2019-02-07 23:44:26 +0300

    корректировки

--- index.php
+++ index.php
@@ -128,20 +128,9 @@ $lots = [
                             <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=$items['price'] ?><b class="rub">р</b></span>
                         </div>
-                        <div class="lot__info">
-                            <span class="lot__category">Название категории</span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">цена<b class="rub">р</b></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
-                                </div>
-                            </div>
+                        <div class="lot__timer timer">
+                            12:23
                         </div>
-
                     </div>
                 </div>
             </li>
