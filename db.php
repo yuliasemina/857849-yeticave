af4268ceedcb968e2ffe382cf24d94761e7a7b86
Yulia <semina.yulia@bk.ru>
2019-02-14 23:12:04 +0300

    запросы из php корректировки 2.0

--- db.php
+++ db.php
@@ -2,13 +2,12 @@
 $link = mysqli_init();
 mysqli_options($link, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1); 
 
-$con = mysqli_connect('localhost', 'root', '', '857849-yeticave');
+$con = mysqli_connect('localhost', 'root', '1', '857849-yeticave');
 
-if ($con == false) {
+if ($con === false) {
 	print('Ошибка подключения: '. mysqli_connect_error());
-}
-else {
-	mysqli_set_charset($con, 'utf8');
+	exit();
 }
 
-?>
+mysqli_set_charset($con, 'utf8');
+
