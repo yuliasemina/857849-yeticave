fcd6744698bcb59df0f868c428c7dd6ae7825ff3
Yulia <semina.yulia@bk.ru>
2019-03-09 18:47:50 +0300

    отправка почты 3

--- getwinner.php
+++ getwinner.php
@@ -13,20 +13,21 @@ $lot_list = get_lot_winner($con);
 
 foreach ($lot_list as $lot) {
 	$lot_id = set_winner($con, intval($lot['id']), intval($lot['user_id']));
-
+print('1');
 	if ($lot_id > 0) {
 		$layout_content = include_template('email.php', [
 			'user_name'=> $lot['user_name'], 
 			'lot'=> $lot['id'], 
 			'title'=> $lot['title']
 		]);
-
+print('2');
 		$message->setTo(['semina.yulia@bk.ru' => 'Юлия']);
+		print('3');
 		$message->setBody($layout_content);
 		// Отправка сообщения
-		$mailer = new Swift_Mailer($transport);
+print('4');
 		$mailer->send($message);
-		
+print('--------------5');
 	}
 
 }
--- templates/email.php
+++ templates/email.php
@@ -1,8 +1,7 @@
 <h1>Поздравляем с победой</h1>
 <p>Здравствуйте, <?= $user_name ?></p>
-<p>Ваша ставка для лота <a href="http://<?= $_SERVER[''] ?>/lot.php?id=<?= $lot ?>"><?= $title ?></a> победила.</p>
-<p>Перейдите по ссылке <a href="http://<?= $_SERVER[''] ?>/mybets.php">мои ставки</a>,
+<p>Ваша ставка для лота <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/lot.php?id=<?= $lot ?>"><?= $title ?></a> победила.</p>
+<p>Перейдите по ссылке <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/mybets.php">мои ставки</a>,
 чтобы связаться с автором объявления</p>
 
 <small>Интернет Аукцион "YetiCave"</small>
-
